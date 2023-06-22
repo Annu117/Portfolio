2b5789981cea947df85efb4eab91329d3071e55b
Annu117 <108427028+Annu117@users.noreply.github.com>
2023-06-22 22:50:22 +0530

    Add files via upload

---
+++ Flipbook/js/config.js
@@ -0,0 +1,12 @@
+CONFIG = {
+    TOTAL_PAGES: 43,
+    IMAGE_PATH: 'images/',
+    ALIVE_PAGES: 5,
+    IMAGE_EXTENSION: '.png'
+}
+
+BB_CONFIG = {
+    speed: 800,
+    shadowsSides: 0.8,
+    shadowFlip: 0.7
+}
