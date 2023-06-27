1e7e6c99e3435dc3d43dc2d07076e294463f6b27
Annu117 <108427028+Annu117@users.noreply.github.com>
2023-06-27 17:20:25 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const text=['A DESIGNER','A DEVELOPER'];
+let count=0,index=0,currentText='',letter='';
+
 (function () {
     [...document.querySelectorAll(".control")].forEach(button => {
         button.addEventListener("click", function() {
@@ -12,3 +15,20 @@
     })
 })();
 
+//self invokes function
+function type(){
+    if(count===text.length)
+    {
+        count=0;    //if printed both words in text then reset count to 0
+    }
+    currentText=text[count];
+    letter=currentText.slice(0,index++);    //take a subset of the string intext till index position
+    
+    document.querySelector('.name .typing').textContent=letter;
+    if(letter.length===currentText.length){
+        count++;    //change pointer to the next element in text array
+        index=0;    //reset index to first letter of the next word
+    }
+    setTimeout(type,400); //calls after 400ms/
+}
+type();
